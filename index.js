8f9cfa57e569a01c576fe6337311d19504bfa610
Halceeyon <IsaacAki247@gmail.com>
2023-08-07 22:21:18 +0100

    Implemented a practice timer and added and an id to the start button
    - Added an id to the start button on index.html
    
    - Created the startbtn variable in index.js assigned it the value of the start buttons id
    
    - Created a new variable el for setting event listeners and preparing handles for the page
    
    - Changed the setTimer function name to startTimer to better reflect what the function does.
    
    - Updated the startTimer function to count down from 30 seconds using the setInterval function
    
    - Created function prepareHandles for setting up page elements. Set up the query selector for the start button
    
    - Created function eventListeners for adding event listeners to buttons. Added event listener 'click' for the start button which calls on the startTimer function when clicked
    
    - Called the functions at the end of the file

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
+const el = {};
 const timerElement = document.getElementById("timer");
+const startbtn = document.getElementById("startbtn");
 
-function setTimer() {
-    // .
+function startTimer() {
+    let seconds = 30;
+    let timer = setInterval(() => {
+        timerElement.innerHTML = '00:' + seconds;
+        seconds--;
+        if (seconds < 0) {
+            clearInterval(timer);
+        }
+    }, 1000);
 }
+
+/** Page elements used are set up here */
+function prepareHandles() {
+    el.startbtn = document.querySelector('#startbtn');
+}
+
+/** Connects listeners for button clicks */
+function eventListeners() {
+    el.startbtn.addEventListener('click', startTimer);
+}
+
+prepareHandles();
+eventListeners();
