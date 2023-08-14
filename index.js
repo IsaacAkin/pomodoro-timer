c0c549fba25cd4702f58fe25ffb18c9ec98aab66
Halceeyon <IsaacAki247@gmail.com>
2023-08-14 14:42:59 +0100

    Revert "Gave functionality to increment and decrement buttons"
    This reverts commit eafdd1b91c1514182f8f21399c224cc5a8d7d66f.

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const el = {};
 const timerElement = document.querySelector('#timer');
 const startBtn = document.querySelector('#startbtn');
-const incrementBtn = document.querySelector('#incrementbtn');
-const decrementBtn = document.querySelector('#decrementbtn');
 
 timerElement.textContent = '00:30';
 
@@ -17,24 +15,6 @@ function startTimer() {
     }, 1000);
 }
 
-/** Increments timer on button click */
-function incrementTimer() {
-    minutes += 5;
-    if (minutes > 50) {
-        minutes = 50;
-    }
-    timerElement.textContent = minutes + ':00';
-}
-
-/** Decrements timer on button click */
-function decrementTimer() {
-    minutes -= 5;
-    if (minutes < 25) {
-        minutes = 25;
-    }
-    timerElement.textContent = minutes + ':00';
-}
-
 function changeToPause() {
     startBtn.textContent = 'Pause';
 }
@@ -42,8 +22,6 @@ function changeToPause() {
 /** Page elements used are set up here */
 function prepareHandles() {
     el.startBtn = document.querySelector('#startbtn');
-    el.incrementBtn = document.querySelector('#incrementbtn');
-    el.decrementBtn = document.querySelector('#decrementbtn');
 }
 
 /** Connects listeners for button clicks */
@@ -52,8 +30,6 @@ function eventListeners() {
         startTimer(); 
         changeToPause()
     });
-    el.incrementBtn.addEventListener('click', incrementTimer);
-    el.decrementBtn.addEventListener('click', decrementTimer);
 }
 
 prepareHandles();
